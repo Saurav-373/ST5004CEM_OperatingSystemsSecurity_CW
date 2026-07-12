124f2abc6c250e724ba3f3468fcb6391996d8923
Saurav Mainali <sauravmainali@83gmail.com>
2026-07-12 18:56:55 +0545

    Add working FIFO and LRU memory management simulator

--- 02_Memory_Management/task2_memory_management.c
+++ 02_Memory_Management/task2_memory_management.c
@@ -2,17 +2,19 @@
 
 #define MAX_REFERENCES 100
 #define MAX_FRAMES 20
-#define EMPTY -1
+#define EMPTY_FRAME -1
 
-void initializeFrames(int frames[], int frameCount) {
+void initializeFrames(int frames[], int frameCount)
+{
     for (int i = 0; i < frameCount; i++) {
-        frames[i] = EMPTY;
+        frames[i] = EMPTY_FRAME;
     }
 }
 
-int searchPage(int frames[], int frameCount, int page) {
+int findPage(int frames[], int frameCount, int pageNumber)
+{
     for (int i = 0; i < frameCount; i++) {
-        if (frames[i] == page) {
+        if (frames[i] == pageNumber) {
             return i;
         }
     }
@@ -20,11 +22,12 @@ int searchPage(int frames[], int frameCount, int page) {
     return -1;
 }
 
-void displayFrames(int frames[], int frameCount) {
+void displayFrames(int frames[], int frameCount)
+{
     printf("[ ");
 
     for (int i = 0; i < frameCount; i++) {
-        if (frames[i] == EMPTY) {
+        if (frames[i] == EMPTY_FRAME) {
             printf("- ");
         } else {
             printf("%d ", frames[i]);
@@ -34,44 +37,45 @@ void displayFrames(int frames[], int frameCount) {
     printf("]");
 }
 
-void printResult(int hits, int faults, int totalReferences) {
-    float hitRatio;
-    float missRatio;
+void displaySummary(int hits, int faults, int totalReferences)
+{
+    float hitRatio = ((float) hits / totalReferences) * 100;
+    float missRatio = ((float) faults / totalReferences) * 100;
 
-    hitRatio = ((float) hits / totalReferences) * 100;
-    missRatio = ((float) faults / totalReferences) * 100;
-
-    printf("\nTotal References : %d\n", totalReferences);
-    printf("Total Hits       : %d\n", hits);
-    printf("Total Faults     : %d\n", faults);
-    printf("Hit Ratio        : %.2f%%\n", hitRatio);
-    printf("Miss Ratio       : %.2f%%\n", missRatio);
+    printf("\nTotal references : %d\n", totalReferences);
+    printf("Page hits        : %d\n", hits);
+    printf("Page faults      : %d\n", faults);
+    printf("Hit ratio        : %.2f%%\n", hitRatio);
+    printf("Miss ratio       : %.2f%%\n", missRatio);
 }
 
-void fifoPageReplacement(int pages[], int referenceCount, int frameCount) {
+void runFIFO(int pages[], int referenceCount, int frameCount)
+{
     int frames[MAX_FRAMES];
-    int nextReplace = 0;
+    int nextFrame = 0;
     int hits = 0;
     int faults = 0;
 
     initializeFrames(frames, frameCount);
 
-    printf("\n=====================================\n");
-    printf("FIFO Page Replacement\n");
-    printf("=====================================\n");
+    printf("\n========================================\n");
+    printf("FIFO PAGE REPLACEMENT\n");
+    printf("========================================\n");
 
     for (int i = 0; i < referenceCount; i++) {
         int currentPage = pages[i];
 
         printf("Page %2d -> ", currentPage);
 
-        if (searchPage(frames, frameCount, currentPage) != -1) {
+        if (findPage(frames, frameCount, currentPage) != -1) {
             hits++;
             printf("Hit   ");
         } else {
             faults++;
-            frames[nextReplace] = currentPage;
-            nextReplace = (nextReplace + 1) % frameCount;
+
+            frames[nextFrame] = currentPage;
+            nextFrame = (nextFrame + 1) % frameCount;
+
             printf("Fault ");
         }
 
@@ -79,32 +83,24 @@ void fifoPageReplacement(int pages[], int referenceCount, int frameCount) {
         printf("\n");
     }
 
-    printResult(hits, faults, referenceCount);
+    displaySummary(hits, faults, referenceCount);
 }
 
-int findLeastRecentlyUsed(int lastUsed[], int frameCount) {
-    int smallestIndex = 0;
+int findLeastRecentlyUsedFrame(int lastUsed[], int frameCount)
+{
+    int leastRecentlyUsedIndex = 0;
 
     for (int i = 1; i < frameCount; i++) {
-        if (lastUsed[i] < lastUsed[smallestIndex]) {
-            smallestIndex = i;
+        if (lastUsed[i] < lastUsed[leastRecentlyUsedIndex]) {
+            leastRecentlyUsedIndex = i;
         }
     }
 
-    return smallestIndex;
+    return leastRecentlyUsedIndex;
 }
 
-int findEmptyFrame(int frames[], int frameCount) {
-    for (int i = 0; i < frameCount; i++) {
-        if (frames[i] == EMPTY) {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
-void lruPageReplacement(int pages[], int referenceCount, int frameCount) {
+void runLRU(int pages[], int referenceCount, int frameCount)
+{
     int frames[MAX_FRAMES];
     int lastUsed[MAX_FRAMES];
     int hits = 0;
@@ -116,33 +112,41 @@ void lruPageReplacement(int pages[], int referenceCount, int frameCount) {
         lastUsed[i] = -1;
     }
 
-    printf("\n=====================================\n");
-    printf("LRU Page Replacement\n");
-    printf("=====================================\n");
+    printf("\n========================================\n");
+    printf("LRU PAGE REPLACEMENT\n");
+    printf("========================================\n");
 
     for (int i = 0; i < referenceCount; i++) {
         int currentPage = pages[i];
-        int pageIndex = searchPage(frames, frameCount, currentPage);
+        int pagePosition = findPage(frames, frameCount, currentPage);
 
         printf("Page %2d -> ", currentPage);
 
-        if (pageIndex != -1) {
+        if (pagePosition != -1) {
             hits++;
-            lastUsed[pageIndex] = i;
+            lastUsed[pagePosition] = i;
+
             printf("Hit   ");
         } else {
-            int replaceIndex;
+            int replacementIndex = -1;
 
             faults++;
 
-            replaceIndex = findEmptyFrame(frames, frameCount);
-
-            if (replaceIndex == -1) {
-                replaceIndex = findLeastRecentlyUsed(lastUsed, frameCount);
+            /* Use an empty frame before replacing an existing page. */
+            for (int j = 0; j < frameCount; j++) {
+                if (frames[j] == EMPTY_FRAME) {
+                    replacementIndex = j;
+                    break;
+                }
             }
 
-            frames[replaceIndex] = currentPage;
-            lastUsed[replaceIndex] = i;
+            if (replacementIndex == -1) {
+                replacementIndex =
+                    findLeastRecentlyUsedFrame(lastUsed, frameCount);
+            }
+
+            frames[replacementIndex] = currentPage;
+            lastUsed[replacementIndex] = i;
 
             printf("Fault ");
         }
@@ -151,99 +155,88 @@ void lruPageReplacement(int pages[], int referenceCount, int frameCount) {
         printf("\n");
     }
 
-    printResult(hits, faults, referenceCount);
+    displaySummary(hits, faults, referenceCount);
 }
 
-void convertAddressesToPages(
-    int addresses[],
-    int pages[],
-    int referenceCount,
-    int pageSizeKB
-) {
-    int pageSizeBytes = pageSizeKB * 1024;
-
-    printf("\nAddress to Page Number Conversion\n");
-    printf("---------------------------------\n");
-
-    for (int i = 0; i < referenceCount; i++) {
-        pages[i] = addresses[i] / pageSizeBytes;
-
-        printf(
-            "Address %d -> Page %d\n",
-            addresses[i],
-            pages[i]
-        );
-    }
-}
-
-int main() {
+int main(void)
+{
     int pageSizeKB;
+    int pageSizeBytes;
     int frameCount;
     int referenceCount;
-    int inputChoice;
-    int rawInput[MAX_REFERENCES];
-    int pages[MAX_REFERENCES];
 
-    printf("Virtual Memory Management Simulator\n");
-    printf("FIFO and LRU Page Replacement in C\n");
-    printf("==================================\n");
+    int logicalAddresses[MAX_REFERENCES];
+    int pageNumbers[MAX_REFERENCES];
+
+    printf("========================================\n");
+    printf("VIRTUAL MEMORY MANAGEMENT SIMULATOR\n");
+    printf("========================================\n");
 
     printf("Enter page size in KB: ");
-    scanf("%d", &pageSizeKB);
 
-    if (pageSizeKB <= 0) {
-        printf("Invalid page size.\n");
+    if (scanf("%d", &pageSizeKB) != 1 || pageSizeKB <= 0) {
+        printf("Error: Page size must be a positive number.\n");
         return 1;
     }
 
-    printf("Enter number of frames: ");
-    scanf("%d", &frameCount);
+    printf("Enter number of memory frames: ");
 
-    if (frameCount <= 0 || frameCount > MAX_FRAMES) {
-        printf("Frame count must be between 1 and %d.\n", MAX_FRAMES);
+    if (scanf("%d", &frameCount) != 1 ||
+        frameCount <= 0 ||
+        frameCount > MAX_FRAMES) {
+        printf(
+            "Error: Frame count must be between 1 and %d.\n",
+            MAX_FRAMES
+        );
         return 1;
     }
 
-    printf("Enter number of references: ");
-    scanf("%d", &referenceCount);
+    printf("Enter number of logical addresses: ");
 
-    if (referenceCount <= 0 || referenceCount > MAX_REFERENCES) {
-        printf("Reference count must be between 1 and %d.\n", MAX_REFERENCES);
+    if (scanf("%d", &referenceCount) != 1 ||
+        referenceCount <= 0 ||
+        referenceCount > MAX_REFERENCES) {
+        printf(
+            "Error: Reference count must be between 1 and %d.\n",
+            MAX_REFERENCES
+        );
         return 1;
     }
 
-    printf("\nChoose input type:\n");
-    printf("1. Page numbers\n");
-    printf("2. Logical addresses\n");
-    printf("Enter choice: ");
-    scanf("%d", &inputChoice);
+    pageSizeBytes = pageSizeKB * 1024;
 
-    if (inputChoice != 1 && inputChoice != 2) {
-        printf("Invalid input choice.\n");
-        return 1;
-    }
-
-    printf("\nEnter the reference string:\n");
+    printf(
+        "Enter %d logical addresses separated by spaces:\n",
+        referenceCount
+    );
 
     for (int i = 0; i < referenceCount; i++) {
-        scanf("%d", &rawInput[i]);
-
-        if (rawInput[i] < 0) {
-            printf("Negative values are not allowed.\n");
+        if (scanf("%d", &logicalAddresses[i]) != 1) {
+            printf("Error: Invalid address entered.\n");
             return 1;
         }
 
-        if (inputChoice == 1) {
-            pages[i] = rawInput[i];
+        if (logicalAddresses[i] < 0) {
+            printf("Error: Logical addresses cannot be negative.\n");
+            return 1;
         }
+
+        pageNumbers[i] = logicalAddresses[i] / pageSizeBytes;
     }
 
-    if (inputChoice == 2) {
-        convertAddressesToPages(rawInput, pages, referenceCount, pageSizeKB);
+    printf("\nAddress to page conversion\n");
+    printf("--------------------------\n");
+
+    for (int i = 0; i < referenceCount; i++) {
+        printf(
+            "Address %6d -> Page %d\n",
+            logicalAddresses[i],
+            pageNumbers[i]
+        );
     }
 
-    fifoPageReplacement(pages, referenceCount, frameCount);
-    lruPageReplacement(pages, referenceCount, frameCount);
+    runFIFO(pageNumbers, referenceCount, frameCount);
+    runLRU(pageNumbers, referenceCount, frameCount);
 
     return 0;
 }
